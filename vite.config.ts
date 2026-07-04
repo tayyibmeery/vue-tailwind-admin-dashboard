bcacabad24dbe087010424b4fa0edd0bf7ef82ec
tayyib meery <tayyibmeerybalti@gmail.com>
2026-07-04 06:07:54 +0500

    erw
    Signed-off-by: tayyib meery <tayyibmeerybalti@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,10 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
+  server: {
+    host: true, // or '0.0.0.0'
+    port: 5173,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
